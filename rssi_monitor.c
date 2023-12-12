bd86aacf1568d930b22b2d9400fb35c434828f92
efeoguslu <efeoguslu@gmail.com>
2023-12-12 14:25:09 +0300

    update comments

--- rssi_monitor.c
+++ rssi_monitor.c
@@ -1,10 +1,12 @@
 /*
-The code snippet is a C program that performs Bluetooth device scanning and connects to a chosen device based on user input. It uses the BlueZ library to interact with the Bluetooth stack.
+Bluetooth Distance Measurement with RSSI
 
-Inputs
-The code snippet takes no inputs from the command line. It prompts the user to choose a device to connect to or to rescan for nearby devices.
+This is a C program that performs Bluetooth device scanning and connects to a chosen device based on user input. It uses the BlueZ library to interact with the Bluetooth stack.
 
-Flow
+Inputs: This program prompts the user to choose a device to connect to or to rescan for nearby devices.
+
+Flow:
+
 1. The program starts by initializing variables and allocating memory for the "inquiry_info" array to store information about discovered devices.
 2. It performs a Bluetooth device inquiry using the "hci_inquiry" function, which retrieves information about nearby devices.
 3. The program then prints the available devices with their MAC addresses and names.
@@ -15,8 +17,8 @@ Flow
 8. If the target device is found, the program executes a system command to get the RSSI (Received Signal Strength Indication) value of the device.
 9. The RSSI is then converted into distance using the "log-distance path loss model" method and printed to the output console.
 
-Outputs
-The program prints the available devices with their MAC addresses and names. If the user chooses a device, the program executes a system command to get the RSSI value of the device.
+Outputs: The program prints the available devices with their MAC addresses and names. If the user chooses a device, the program executes a system command to get the RSSI value of the device. 
+It also prints the distance according to the RSSI value.
 */
 
 #include <stdio.h>
